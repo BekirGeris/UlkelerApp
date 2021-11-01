0ed8d7bff5dbe3ff0a4fe0c3ee2c908a6587d8f2
bekirgeris <bekirgeris@marun.edu.tr>
2021-11-01 23:03:55 +0300

    +view -> Country fragment

--- app/src/main/java/com/begers/ulkeler/view/FeedFragment.kt
+++ app/src/main/java/com/begers/ulkeler/view/FeedFragment.kt
@@ -5,9 +5,13 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.begers.ulkeler.R
 import com.begers.ulkeler.adapter.CountryAdapter
 import com.begers.ulkeler.viewmodel.FeedViewModel
+import kotlinx.android.synthetic.main.fragment_feed.*
 
 class FeedFragment : Fragment() {
 
@@ -26,6 +30,52 @@ class FeedFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        viewModel = ViewModelProviders.of(this).get(FeedViewModel::class.java)
+
+        viewModel.refreshData()
+
+        countryList.layoutManager = LinearLayoutManager(context)
+        countryList.adapter = countryAdapter
+
+        observeLiveData()
     }
 
+
+    fun observeLiveData(){
+
+        viewModel.countries.observe(viewLifecycleOwner, Observer { countries ->
+            countries?.let {
+                countryList.visibility = View.VISIBLE
+                countryAdapter.updateCountryList(countries)
+            }
+        })
+
+        viewModel.countryError.observe(viewLifecycleOwner, Observer { error ->
+            error?.let {
+                if (it){
+                    //hata mesajı varsa göster
+                    countryError.visibility = View.VISIBLE
+                    countryLoading.visibility = View.GONE
+                    countryList.visibility = View.GONE
+                }else{
+                    //hata mesajı yok gösterme
+                    countryError.visibility = View.GONE
+                }
+            }
+        })
+
+        viewModel.countryLoading.observe(viewLifecycleOwner, Observer { loading ->
+            loading?.let {
+                if (it){
+                    //hata mesajı varsa göster
+                    countryLoading.visibility = View.VISIBLE
+                    countryList.visibility = View.GONE
+                    countryError.visibility = View.GONE
+                }else{
+                    //hata mesajı yok gösterme
+                    countryLoading.visibility = View.GONE
+                }
+            }
+        })
+    }
 }
